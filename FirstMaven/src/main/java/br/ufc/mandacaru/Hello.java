424fe5b1bb739733d043b668c50535eb8fc5350d
João Victor Carvalho <joaocarvoli@hotmail.com>
2022-07-22 16:05:00 -0300

    servlet configuration

--- FirstMaven/src/main/java/br/ufc/mandacaru/Hello.java
+++ FirstMaven/src/main/java/br/ufc/mandacaru/Hello.java
@@ -1,9 +1,16 @@
 package br.ufc.mandacaru;
 
+import br.ufc.mandacaru.model.*;
+import br.ufc.mandacaru.config.ThymeleafConfig;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.WebContext;
+import org.thymeleaf.web.IWebExchange;
+import org.thymeleaf.web.servlet.JakartaServletWebApplication;
+
 import java.io.IOException;
 
 /**
@@ -11,28 +18,23 @@ import java.io.IOException;
  */
 public class Hello extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
     public Hello() {
         super();
         // TODO Auto-generated constructor stub
     }
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		TemplateEngine templateEngine = (TemplateEngine) getServletContext().getAttribute(ThymeleafConfig.TEMPLATE_ENGINE_ATTR);
+		IWebExchange webExchange = JakartaServletWebApplication.buildApplication(getServletContext()).buildExchange(request, response);
+		WebContext context = new WebContext(webExchange);
+
+		Address address = new Address("Rua Zacarias Carlos de Melo" , "63700-000");
+		Person person = new Person(1, "Almada", address);
+		context.setVariable("person", person);
+		templateEngine.process("hello", context, response.getWriter());
 	}
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
 		doGet(request, response);
 	}
 
--- FirstMaven/src/main/java/br/ufc/mandacaru/config/ThymeleafConfig.java
+++ FirstMaven/src/main/java/br/ufc/mandacaru/config/ThymeleafConfig.java
@@ -1,6 +1,49 @@
 package br.ufc.mandacaru.config;
 
+import jakarta.servlet.ServletContextEvent;
 import jakarta.servlet.ServletContextListener;
+import jakarta.servlet.annotation.WebListener;
+import org.thymeleaf.ITemplateEngine;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.templatemode.TemplateMode;
+import org.thymeleaf.templateresolver.WebApplicationTemplateResolver;
+import org.thymeleaf.web.IWebApplication;
+import org.thymeleaf.web.servlet.JakartaServletWebApplication;
 
+@WebListener
 public class ThymeleafConfig implements ServletContextListener {
+    public static final String TEMPLATE_ENGINE_ATTR = "TemplateEngineInstance";
+    private ITemplateEngine templateEngine;
+    private JakartaServletWebApplication application;
+
+    @Override
+    public void contextInitialized(ServletContextEvent sce){
+        this.application = JakartaServletWebApplication.buildApplication(sce.getServletContext());
+        this.templateEngine = templateEngine(this.application);
+
+        sce.getServletContext().setAttribute(TEMPLATE_ENGINE_ATTR, templateEngine);
+    }
+
+    private ITemplateEngine templateEngine(IWebApplication application) {
+        TemplateEngine templateEngine = new TemplateEngine();
+        WebApplicationTemplateResolver templateResolver = templateResolver(application);
+        templateEngine.setTemplateResolver(templateResolver);
+
+        return templateEngine;
+    }
+
+    private WebApplicationTemplateResolver templateResolver(IWebApplication application) {
+        WebApplicationTemplateResolver templateResolver = new WebApplicationTemplateResolver(application);
+
+        templateResolver.setTemplateMode(TemplateMode.HTML);
+        templateResolver.setPrefix("/WEB-INF/templates/");
+        templateResolver.setSuffix(".html");
+
+        return templateResolver;
+    }
+
+    @Override
+    public void contextDestroyed(ServletContextEvent sce){
+
+    }
 }
