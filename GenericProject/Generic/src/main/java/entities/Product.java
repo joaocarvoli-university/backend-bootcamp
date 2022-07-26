d313eb789babbd84ae9c05ac839663e2f6aa35fb
João Victor Carvalho <joaocarvoli@hotmail.com>
2022-07-26 12:11:44 -0300

    Adapting product class to use the Hibernate ORM

--- GenericProject/Generic/src/main/java/entities/Product.java
+++ GenericProject/Generic/src/main/java/entities/Product.java
@@ -1,6 +1,13 @@
 package entities;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+@Entity(name = "products")
 public class Product {
+    @Id
+    @GeneratedValue
     private int id;
     private String name;
     private double price;
