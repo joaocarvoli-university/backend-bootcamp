a5e45da4b37be1b8ffa25bf33dd11e85abda1d87
João Victor Carvalho <joaocarvoli@hotmail.com>
2022-07-27 19:16:28 -0300

    test file

---
+++ activities/work-01/src/main/java/app/Main.java
@@ -0,0 +1,11 @@
+package app;
+
+import app.entities.Property;
+
+public class Main {
+    public static void main(String[] args) {
+        Property prop = new Property();
+        System.out.println(prop);
+        System.out.println(Property.class);
+    }
+}
