d2d8a12468ffe2d9f774bd3dcc795221590fbf13
João Victor Carvalho <joaocarvoli@hotmail.com>
2022-08-04 12:17:37 -0300

    Class that runs the server

---
+++ src/main/java/br/mandacaru/class04/Class04Application.java
@@ -0,0 +1,13 @@
+package br.mandacaru.class04;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Class04Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Class04Application.class, args);
+	}
+
+}
